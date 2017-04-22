93f84e9c4976ee6b5a163997098cf0c1bc6d99f6
WillChurch <wfc2111@columbia.edu>
2017-04-22 14:13:36 -0400

    heavy model balancing

--- CubeEdgeHeavy.m
+++ CubeEdgeHeavy.m
@@ -9,13 +9,14 @@ rb = .0950214;                 %radius from pivot to COM   %m
 
 mm = .110; %.296; %.300;              %motor mass         %kg
 mw = .273185;                     %mass of wheel      %kg
-mb = .5;%.475344; %.419;              %body mass          %kg
-%The .5 accounts for the mcu
+mbH = (1.62778-mw) + (.5-.475344); %.419;              %body mass          %kg
+%The .5 accounts for the mcu etc
 mmw = mw + mm;                 %mass of wheel plus motor %kg  
 
 Im = 1.35e-5; %5.4*10^-6;         %motor inertia      %kg*m^2  ??????????
-Ib = 0.006611 + (.5-.475344)*rb^2; 
+IbH = 0.017274 + (.5-.475344)*rb^2; 
 %additional term is for mcu
+
 %((1/12)*mb*(lb^2+wb^2)) + (mb*rb^2); %.00177408 ;       	%kg*m^2             %kg*m^2
 Iw = 0.00047 + Im;  %.00043835 + Im;    %kg*m^2             %kg*m^2
 
@@ -26,28 +27,28 @@ g = 9.81 ;              %m*s^2
 
 Km = 25.1e-3; %.45/2 ;            %N*m/A    
 
-A = [ 0 1 0 
-    ((mb*rb + mw*l)*g/(Ib+mmw*l^2)) (-Cb/(Ib+mmw*l^2)) (Cw/(Ib+mmw*l^2))
-    (-(mb*rb + mw*l)*g/(Ib+mmw*l^2)) (Cb/(Ib+mmw*l^2)) (-Cw*(Ib+Iw+mmw*l^2)/(Iw*(Ib+mmw*l^2)))];  
+AH = [ 0 1 0 
+    ((mbH*rb + mw*l)*g/(IbH+mmw*l^2)) (-Cb/(IbH+mmw*l^2)) (Cw/(IbH+mmw*l^2))
+    (-(mbH*rb + mw*l)*g/(IbH+mmw*l^2)) (Cb/(IbH+mmw*l^2)) (-Cw*(IbH+Iw+mmw*l^2)/(Iw*(IbH+mmw*l^2)))];  
 
-B = [ 0
-    (-Km/(Ib+mmw*l^2))
-    ((Km*(Ib+Iw+mmw*l^2))/(Iw*(Ib+mmw*l^2)))];
+BH = [ 0
+    (-Km/(IbH+mmw*l^2))
+    ((Km*(IbH+Iw+mmw*l^2))/(Iw*(IbH+mmw*l^2)))];
 
-C = eye(3);
+CH = eye(3);
 
-D = zeros(3,1);
+DH = zeros(3,1);
 
-pen = ss(A,B,C,D);
+pen = ss(AH,BH,CH,DH);
 
-M = [B A*B (A^2)*B];
-rank(M);
+MH = [BH AH*BH (AH^2)*BH];
+rank(MH);
 
 %  J = [-2+2*sqrt(3)*i 0 0;
 %       0 -2-2*sqrt(3)*i 0;
 %       0 0 -10] 
   
-P = [-2.5+2*sqrt(3)*i -2.5-2*sqrt(3)*i -10]'
+PH = [-2.5+2*sqrt(3)*i -2.5-2*sqrt(3)*i -5]'
 
 %  
 % JJ = poly(J);67
@@ -58,14 +59,14 @@ P = [-2.5+2*sqrt(3)*i -2.5-2*sqrt(3)*i -10]'
 % 
 % AA = A - B*K;
 
-K = place(A,B,P);
+KH = place(AH,BH,PH)
 
-Q = [1 0 0
-    0 1 0
-    0 0 1];
-R = 1000;
+QH = [1 0 0
+    0 0 0
+    0 0 0];
+RH = 100000000000000;
 
-[KLQR, S, e] = lqr(A,B,Q,R);
-KLQR
-e
-open_system('SSModel')
+[KLQRH, SH, eH] = lqr(AH,BH,QH,RH);
+KLQRH
+eH
+%open_system('SSModelHeavy')
